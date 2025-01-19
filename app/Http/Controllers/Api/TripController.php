f2287d58e06ca2ca8113ecb1fbf787da7430fdd4
Fabiana Pereira <fabianaduarte980@gmail.com>
2025-01-18 23:33:02 -0300

    fix: join a trip

--- app/Http/Controllers/Api/TripController.php
+++ app/Http/Controllers/Api/TripController.php
@@ -59,15 +59,16 @@ class TripController extends Controller
         return response()->json(compact('trip'), 201);
     }
 
-    public function joinTrip(string $id, Request $request): JsonResponse
+    public function joinTrip(string $id): JsonResponse
     {
+        $trip = Trip::findOrFail($id);
         $user = JWTAuth::parseToken()->authenticate();
 
-        $trip = Trip_participant::create([
+        Trip_participant::create([
             'id_user' => $user->id,
-            'id_trip' => $id
+            'id_trip' => $trip->id
         ]);
-        return response()->json(compact('trip'), 201);
+        return response()->json(['trip_id' => $trip->id], 200);
     }
 
     public function getTrips()
@@ -87,11 +88,19 @@ class TripController extends Controller
         return response()->json($trip);
     }
 
-    public function getParticipants(string $id)
+    public function previewTrip(string $code)
+    {
+        $trip = Trip::where('code', $code)->first();
+        $participants = $this->getParticipants($trip->id);
+        $trip->participants = $participants;
+        return response()->json($trip);
+    }
+
+    protected function getParticipants(string $id)
     {
         $usersIds = Trip_participant::where('id_trip', $id)->pluck('id_user');
         $users = User::whereIn('id', $usersIds)->select('name', 'image_path')->get();
-        return response()->json($users);
+        return $users;
     }
 
     public function updateTrip(Request $request, string $id)
